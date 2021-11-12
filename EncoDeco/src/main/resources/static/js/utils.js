e90cbb7539df2eb97720e84c66ddb69a4801e43a
MkTech-0-8YT3 <mktech@protonmail.com>
2021-11-12 11:09:24 +0100

    Add flag unlock on frontend + small front refactor

--- EncoDeco/src/main/resources/static/js/utils.js
+++ EncoDeco/src/main/resources/static/js/utils.js
@@ -1,11 +1,40 @@
 function encodeData() {
-    var formDataToParse = $("#encodingForm").serializeArray();
+    var formData = parseFormData("#encodingForm");
+    $.ajax("/encode", { data: JSON.stringify(formData), contentType: 'application/json', type:'POST'}).then(function (value) {
+        alert(value);
+    });
+}
+
+function getFlag() {
+    document.getElementById("encodedFlagText").innerText="";
+    $.ajax("/flag", { type:'GET'}).then(function (value) {
+        document.getElementById("encodedFlagText").innerText=value;
+    });
+}
+
+function unlockFlag() {
+    var formData = parseFormData("#keyForm");
+    $.ajax("/unlock", { data: JSON.stringify(formData), contentType: 'application/json', type:'POST'}).then(function (value) {
+        document.getElementById("encodedFlagText").innerText=value;
+    });
+}
+
+function parseFormData(formName) {
+    var formDataToParse = $(formName).serializeArray();
     var parsedData = {};
     $.each(formDataToParse,
         function(i, v) {
             parsedData[v.name] = v.value;
         });
-    $.ajax("/encode", { data: JSON.stringify(parsedData), contentType: 'application/json', type:'POST'}).then(function (value) {
-        alert(value);
+    return parsedData;
+}
+
+function disableKeypressSubmit() {
+    $(document).on("keypress", 'form', function (e) {
+        var code = e.keyCode || e.which;
+        if (code === 13) {
+            e.preventDefault();
+            return false;
+        }
     });
 }
